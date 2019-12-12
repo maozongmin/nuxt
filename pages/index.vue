b9ecb6e75eff56633b35e779f28959a8bfe8a46a
v_zmmao <v_zmmao@tencent.com>
2019-12-12 17:17:06 +0800

    1

--- pages/index.vue
+++ pages/index.vue
@@ -18,6 +18,11 @@
                 </a>
             </div>
         </div>
+        <el-pagination
+        background
+        layout="prev, pager, next"
+        :total="1000">
+        </el-pagination>
     </div>
 </template>
 
